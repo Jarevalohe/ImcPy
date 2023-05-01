7b3098ea1d9ebbb80d0d2c4b4585193268a67823
Juliare1106 <56942969+Juliare1106@users.noreply.github.com>
2023-04-30 19:10:14 -0500

    Ajustes finales

--- view.py
+++ view.py
@@ -1,8 +1,8 @@
 class View:
-        def _init_(self) -> None:
-            self.weight = 0
-            self.height = 0   
-            print("Ingrese su peso en kilogramos")
-            self.weight = input()
-            print("Ingrese su altura en metros")
-            self.height = input()
+    def __init__(self):
+        self.weight = 0
+        self.height = 0   
+        print("Ingrese su peso en kilogramos")
+        self.weight = float(input())
+        print("Ingrese su altura en metros")
+        self.height = float(input())
